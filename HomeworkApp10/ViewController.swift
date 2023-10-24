8e038f67e36a97ca328a4c39681a3380e6aed3ba
Алексей <107575283+alexey4050@users.noreply.github.com>
2023-10-24 18:29:32 +0300

    Initial Commit

---
+++ HomeworkApp10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeworkApp10
+//
+//  Created by testing on 24.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
